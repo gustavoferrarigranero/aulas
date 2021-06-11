38b0b7265cef46105c82be8830dd1481bb6653ff
Gustavo Ferrari Granero <gustavoferrarigranero@gmail.com>
2021-06-11 21:22:25 +0000

    Primeira aula

---
+++ aula1.js
@@ -0,0 +1,15 @@
+//Variaveis
+
+const numero = 15;
+
+if (numero > 10 && numero == 15) {
+    console.log('É maior que 10 e igual a 15!!!')
+} else {
+    console.log('Numero inválido!!!')
+}
+
+if (numero > 10 && numero == 15) {
+    console.log('É maior que 10 e igual a 15!!!')
+} else {
+    console.log('Numero inválido!!!')
+}
---
+++ exercicio1.js
@@ -0,0 +1,25 @@
+// Perguntar pro usuário qual numero vai ser testado
+// Pode inserir qualquer número
+const numero = 15;
+
+
+// Exercicio 1 testar se o numero é maior que 10
+if (numero > 10)
+    console.log('É maior que 10!!!')
+else
+    console.log('Não é maior que 10!!!')
+
+
+// Exercicio 2 testar se o numero é igual(==) a 10
+
+
+// Exercicio 3 testar se o numero é maior ou igual (>=) a 10
+
+
+// Exercicio 4 testar se o numero é menor (<) que 20
+
+
+// Exercicio 5 testar se o numero é menor ou igual (<=) que 10
+
+
+// Exercicio 6 testar se o numero é igual (==) a 30
