1892cb9ff124373a9b19bff880181edf6678d414
Gustavo Ferrari Granero <gustavoferrarigranero@gmail.com>
2021-06-12 15:43:10 +0000

    exercicios 1

--- exercicio1.js
+++ exercicio1.js
@@ -11,15 +11,38 @@ else
 
 
 // Exercicio 2 testar se o numero é igual(==) a 10
+if (numero == 10)
+    console.log('É igual a 10 ')
+    else
+    console.log('Não é igual a 10 ')
+
 
 
 // Exercicio 3 testar se o numero é maior ou igual (>=) a 10
 
 
-// Exercicio 4 testar se o numero é menor (<) que 20
+if (numero >= 10)
+    console.log('É maior ou igual a 10!!!  ')
+else
+    console.log('Não é maior nem igual  que 10!!! ')
 
+
+// Exercicio 4 testar se o numero é menor (<) que 20
+if (numero < 20)
+    console.log('É menor que 20!!!')
+else
+    conosle.log('Não é menor que 20!!! ')
 
 // Exercicio 5 testar se o numero é menor ou igual (<=) que 10
-
+if (numero <= 10)
+    conosle.log('É menor ou igual que 10!!')
+else
+    console.log('Não é menor ou igual a  igual a 10!!')
 
 // Exercicio 6 testar se o numero é igual (==) a 30
+if (numero == 30)
+    console.log('É igual a 30 !!')
+else
+    console.log('Não é igual a 30!!')
+
+    
