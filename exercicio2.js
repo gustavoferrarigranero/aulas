ef031f0253cebcea1304f1238423fdaf2d326fc2
Gustavo Ferrari Granero <gustavoferrarigranero@gmail.com>
2021-06-12 16:40:03 +0000

    exercicio 2

---
+++ exercicio2.js
@@ -0,0 +1,10 @@
+// Exercicio 1 criar um Array de uma casa, 
+// contendo dentro deles os nomes dos comodos da casa
+// Percorra esse array e printe os comodos que foram criados com a função FOR
+
+
+
+
+// Exercicio 2 criar um Array de idades de 1 a 15 , 
+// contendo dentro deles as idades de alguém(1,2,3,4...at;e 15)
+// Percorra esse array e printe as idades que foram criados com a função While
